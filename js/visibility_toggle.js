2e2100cd557cd5362684672fb2ac0633c2d02258
Trinity Li <trinitycaimanli@gmail.com>
2023-12-20 03:46:50 -0500

    adding language, formatting, qol

---
+++ js/visibility_toggle.js
@@ -0,0 +1,7 @@
+
+function toggleVisibility(){
+    var viewbtn = document.getElementById('upArrow');
+    viewbtn.style.visibility = (viewbtn.style.visibility === 'visible');
+    console.log('worked');
+    return false;
+}
